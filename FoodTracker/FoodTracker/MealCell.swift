e4f2acf917743d7369988ca7914a3084469ce9e9
郑少朋 <911917@nd.com>
2019-08-21 15:36:11 +0800

    新增列表

---
+++ FoodTracker/FoodTracker/MealCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealCell.swift
+//  FoodTracker
+//
+//  Created by zsp on 2019/8/21.
+//  Copyright © 2019 woop. All rights reserved.
+//
+
+import UIKit
+
+class MealCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
