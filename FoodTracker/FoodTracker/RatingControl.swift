fa0fa5f3f8ae4d8bbf24309c98447421eda1f3a2
郑少朋 <911917@nd.com>
2019-08-21 11:53:24 +0800

    初步实现自定义视图

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by zsp on 2019/8/21.
+//  Copyright © 2019 woop. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
