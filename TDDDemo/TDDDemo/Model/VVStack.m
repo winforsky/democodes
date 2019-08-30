5c0f471f2ade4b48ee5150050d5559ffda0f110e
郑少朋 <911917@nd.com>
2019-08-30 16:12:44 +0800

    TDD转BDD

--- TDDDemo/TDDDemo/Model/VVStack.m
+++ TDDDemo/TDDDemo/Model/VVStack.m
@@ -38,3 +38,20 @@
 }
 
 @end
+
+//BDD其实就是在讲故事。
+//一个典型的BDD的测试用例包活完整的三段式上下文，
+//测试大多可以翻译为Given..When..Then的格式，读起来轻松惬意。
+//describe(@"Team", ^{
+//    context(@"when newly created", ^{
+//        it(@"has a name", ^{
+//            id team = [Team team];
+//            [[team.name should] equal:@"Black Hawks"];
+//        });
+//        
+//        it(@"has 11 players", ^{
+//            id team = [Team team];
+//            [[[team should] have:11] players];
+//        });
+//    });
+//});
